2ac486fc027d802dae9c55a417be9fb96340d37b
ajr-cabbage <alexander.rakotz@gmail.com>
2026-04-08 21:37:17 -0400

    finish troubleshooting

--- src/main.py
+++ src/main.py
@@ -7,8 +7,9 @@ from functions import copy_dir_contents, generate_page_recursive
 def main():
     basepath = "/"
     args = sys.argv
-    if args[0]:
-        basepath = args[0]
+    if len(args) > 1:
+        basepath = args[1]
+    print(f"BASEPATH: {basepath}")
     source_dir = os.path.abspath("./static")
     target_dir = os.path.abspath("./docs")
     copy_dir_contents(source_dir, target_dir)
